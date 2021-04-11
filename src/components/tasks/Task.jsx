580628c6482bf7a7a8c1c95b849a27b37de4faef
hossam <hosamabdulmaged@gmail.com>
2021-04-11 17:49:50 +0200

    updated Icons

--- src/components/tasks/Task.jsx
+++ src/components/tasks/Task.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import { FaTimes } from 'react-icons/fa'
+import { FcAlarmClock } from "react-icons/fc";
+import { FiEdit3 } from "react-icons/fi";
+
 const Task = ({ task, onDelete, onToggle }) => {
 
     return (
-        <div className={`task ${task.reminder ? 'reminder' : ''}`} onDoubleClick={() => onToggle(task.id)}>
+        <div className={`task ${task.reminder ? 'reminder text-center conatiner' : ' text-center'}`} onDoubleClick={() => onToggle(task.id)}>
             <h3>
-                {task.text}
+                <FiEdit3 style={{ display: 'inline' }} />{task.text}
                 <FaTimes style={{ color: 'red', cursor: 'pointer' }}
                     onClick={() => onDelete(task.id)} />
             </h3>
             <p>
-                {task.day.toString()}
+                <FcAlarmClock style={{ display: 'inline' }} /> {task.day.toString()}
             </p>
         </div>
     )
